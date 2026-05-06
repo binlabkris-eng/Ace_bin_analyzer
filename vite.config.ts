2f3da6f97b25e461482556b954515e3b21cccad7
Kris <kris@users.noreply.github.com>
2026-05-06 16:33:17 +0200

    Fix: move Vitest config out of Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Ace_bin_analyzer/",
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-  },
 });
 
